cd2f1f53a3a9787c400754feeb6d06dfc03f1c83
Karol Arvizu <lorakarvizu@gmail.com>
2026-03-24 18:27:32 -0600

    primer commit

---
+++ src/main/java/com/example/ticketinios/jirapobre/JirapobreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketinios.jirapobre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JirapobreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JirapobreApplication.class, args);
+	}
+
+}
